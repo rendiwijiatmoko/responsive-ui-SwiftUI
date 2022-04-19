897413e63da8de13ca346be135c695a011bef865
Rendi Wijiatmoko <rendiwijiatmoko97@gmail.com>
2022-04-19 18:07:55 +0700

    Initial Commit

---
+++ ResponsiveUI/ResponsiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResponsiveUIApp.swift
+//  ResponsiveUI
+//
+//  Created by Rendi Wijiatmoko on 19/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ResponsiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
